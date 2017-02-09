f2f26a8c9815ea7d597497bd804732dceecbd1d8
jefQuery <jeffrey.milberger@gmail.com>
2017-02-09 10:50:05 -0800

    Pass linkedList tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+
+    if (list.tail !== null) {
+      list.tail.next = node;
+    }
+
+    list.tail = node;
+
+    if (list.head === null) {
+      list.head = node;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var headValue = list.head.value;
+      list.head = list.head.next;
+      return headValue;
+    } else {
+      return null;
+    }
   };
 
   list.contains = function(target) {
+    if (list.head !== null) {
+      // iterate through head values
+      var iNode = list.head;
+      while (iNode) {
+        if (iNode.value === target) {
+          return true;
+        }
+        iNode = iNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
