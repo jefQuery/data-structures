e99a904961bd90e1c80dafa42a0cb7de47590667
jefQuery <jeffrey.milberger@gmail.com>
2017-02-08 16:42:16 -0800

    fix puedoclassical queue tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,14 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this._size = 0;
+  this._storage = {};
 };
 
-
+Stack.prototype.push = function(value) {
+  this._storage[this._size++] = value;
+};
+Stack.prototype.pop = function() {
+  return this._storage[this._size = Math.max(0, --this._size)];
+};
+Stack.prototype.size = function() {
+  return this._size;
+};
