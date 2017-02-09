e4c051c050a003c0f2c34b13e5519824254bc55b
jefQuery <jeffrey.milberger@gmail.com>
2017-02-08 19:54:53 -0800

    Pass all Bare Minimum Requirements tests, inc. pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,19 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._first = 0;
+  this._last = 0;
+  this._storage = {};
+};
+
+Queue.prototype.enqueue = function(value) {
+  this._storage[this._last++] = value;
+};
+Queue.prototype.dequeue = function() {
+  return this._first < this._last ? this._storage[this._first++] : null;
+};
+Queue.prototype.size = function() {
+  return this._last - this._first;
 };
 
 
