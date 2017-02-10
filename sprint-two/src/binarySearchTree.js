69d4b0459e6d8c273839aad884f518288b813eab
jefQuery <jeffrey.milberger@gmail.com>
2017-02-10 11:15:26 -0800

    pass binary search tree tests and comment time complexities

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,61 @@
 var BinarySearchTree = function(value) {
+  var treeNode = {};
+  treeNode.left = null;
+  treeNode.right = null;
+  treeNode.value = value;
+
+  treeNode.insert = function(value) {
+    if (value > treeNode.value) {
+      if (treeNode.right === null) {
+        treeNode.right = BinarySearchTree(value);
+      } else {
+        treeNode.right.insert(value);
+      }
+    } else if (value < treeNode.value) {
+      if (treeNode.left === null) {
+        treeNode.left = BinarySearchTree(value);
+      } else {
+        treeNode.left.insert(value);
+      }
+    }
+  };
+
+  treeNode.contains = function(value) {
+    if (value > treeNode.value) {
+      if (treeNode.right === null) {
+        return false;
+      } else {
+        return treeNode.right.contains(value);
+      }
+    } else if (value < treeNode.value) {
+      if (treeNode.left === null) {
+        return false;
+      } else {
+        return treeNode.left.contains(value);
+      }
+    } else {
+      return true;
+    }
+  };
+
+  treeNode.depthFirstLog = function(cb) {
+    cb(treeNode.value);
+    if (treeNode.left !== null) {
+      treeNode.left.depthFirstLog(cb);
+    }
+    if (treeNode.right !== null) {
+      treeNode.right.depthFirstLog(cb);
+    }
+  };
+
+  //var TreeNode = function(value)
+  return treeNode;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+insert: Θ(log n), O(n)
+contains: Θ(log n), O(n)
+depthFirstLog: O(n)
  */
