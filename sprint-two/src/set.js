7ff5f8f42093663890ec045949a07c90a396d288
jefQuery <jeffrey.milberger@gmail.com>
2017-02-09 20:02:52 -0800

    pass all set tests and write time complexities.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return !!this._storage[item];
 };
 
 setPrototype.remove = function(item) {
+  this._storage[item] = null;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add: O(1) (effectively)
+ contains: O(1) (effectively)
+ remove: O(1) (effectively)
  */
